45b871c3a970b9426ed8bd1f6237931049de637b
venkat <srivenkatsairaj13@gmail.com>
2020-12-27 17:52:07 +0530

    Exception handling file added

---
+++ Exception_handling.py
@@ -0,0 +1,8 @@
+#  the first statement which might rasie and exception should be given in try block
+# the code that needs to be excuted in case of the try block fails is given in except block
+
+try:
+    f=open('sample.txt')
+except:
+    print('The file your trying to access is not found !')
+#  there instead of giving a run time error , once the try block failed the except block is executed which alerts the user about the error that happened above ( this is just an example , not everytime the  except block needs to have this).
