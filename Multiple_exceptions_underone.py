e44935f80f877ddcdd05fa63ff722a7ff7a4c3fe
venkat <srivenkatsairaj13@gmail.com>
2021-01-01 19:13:07 +0530

    modified example

--- Multiple_exceptions_underone.py
+++ Multiple_exceptions_underone.py
@@ -1,10 +1,14 @@
 #  there is also a way to handle multiple exceptions in one except block 
-
+print("hello")
 try:
-    a=b
+    # a=b
     print( "breaked here")
-    z=10/0
+    # z=10/0
+    f=open('sample.txt')
 
 except (NameError,ZeroDivisionError):
     print('Some error has occured ')
 #  just like in the single exception handling case it wont move to next line when and exception is encountered.
+#  if the exception is not caught in the above cases then it moves to another except block if provided.
+except:
+    print('Error')
